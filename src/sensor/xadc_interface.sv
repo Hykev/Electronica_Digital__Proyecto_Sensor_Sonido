b5d415a195ed39022cacb05b6ade4c74b770c996
Hykev <kggonzalez@unis.edu.gt>
2026-04-05 22:49:23 -0600

    top and hardware implementation files

--- src/sensor/xadc_interface.sv
+++ src/sensor/xadc_interface.sv
@@ -1,26 +1,30 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04.04.2026 18:58:49
-// Design Name: 
-// Module Name: xadc_interface
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
+`timescale 1ns/1ps
 
 module xadc_interface(
 
-    );
+    input  logic clk,
+    input  logic reset,
+
+    output logic [11:0] adc_data,
+    output logic sample_ready
+
+);
+
+logic [11:0] counter;
+
+always_ff @(posedge clk or posedge reset) begin
+
+    if(reset) begin
+        counter <= 0;
+        sample_ready <= 0;
+    end
+    else begin
+        counter <= counter + 1;
+
+        adc_data <= counter;
+        sample_ready <= 1;
+    end
+
+end
+
 endmodule
--- src/top/sound_monitor_top.sv
+++ src/top/sound_monitor_top.sv
@@ -1,26 +1,71 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04.04.2026 18:28:35
-// Design Name: 
-// Module Name: sound_monitor_top
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
+`timescale 1ns/1ps
 
 module sound_monitor_top(
 
-    );
+    input  logic clk,
+    input  logic reset,
+
+    output logic [6:0] seg
+
+);
+
+logic [11:0] adc_data;
+logic sample_ready;
+
+logic take_sample;
+logic accumulate;
+logic compute;
+logic update_display;
+
+logic [15:0] accumulated_value;
+logic [3:0] level_digit;
+
+xadc_interface xadc(
+
+    .clk(clk),
+    .reset(reset),
+    .adc_data(adc_data),
+    .sample_ready(sample_ready)
+
+);
+
+control_fsm fsm(
+
+    .clk(clk),
+    .reset(reset),
+    .sample_ready(sample_ready),
+    .level_high(1'b0),
+
+    .take_sample(take_sample),
+    .accumulate(accumulate),
+    .compute(compute),
+    .update_display(update_display)
+
+);
+
+accumulator acc(
+
+    .clk(clk),
+    .reset(reset),
+    .enable(accumulate),
+    .clear(1'b0),
+    .sample_in(adc_data),
+    .sum_out(accumulated_value)
+
+);
+
+level_decoder decoder(
+
+    .sound_level(accumulated_value),
+    .level_digit(level_digit)
+
+);
+
+sevenseg_driver display(
+
+    .digit(level_digit),
+    .seg(seg)
+
+);
+
 endmodule
